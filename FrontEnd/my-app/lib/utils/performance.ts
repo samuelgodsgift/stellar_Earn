725860f1b291a0cf5f0b536a19d2e0d6d25e8829
Oluwatos94 <akinbowat@gmail.com>
2026-03-24 03:58:13 +0100

    Redesign Homepage - How It Works Section

--- FrontEnd/my-app/lib/utils/performance.ts
+++ FrontEnd/my-app/lib/utils/performance.ts
@@ -30,7 +30,7 @@ export const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
     if (onPerfEntry && onPerfEntry instanceof Function) {
         import('web-vitals').then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
             onCLS(onPerfEntry);
-            onFID(onPerfEntry);
+            // onFID(onPerfEntry);
             onFCP(onPerfEntry);
             onLCP(onPerfEntry);
             onTTFB(onPerfEntry);
